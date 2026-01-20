3831abbf0161c3ae479735393d6d773d38189289
RAMTIN KORSHIDI <rkhorshidi2003@gmail.com>
2026-01-20 16:12:05 -0700

    A11y: Improve accessibility for Earth Animation component

---
+++ src/components/EarthGlobe.tsx
@@ -0,0 +1,14 @@
+import './EarthAnimation.css';
+
+const EarthGlobe = () => {
+    return (
+        <div
+            className="earth-globe"
+            role="img"
+            aria-label="Animated rotating earth globe"
+            title="Global availability"
+        ></div>
+    );
+};
+
+export default EarthGlobe;
